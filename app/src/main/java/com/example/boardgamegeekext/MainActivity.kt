0edd33eec107990a8e4d3f1b0ab96160918dedbb
Marcin <marcinkrueger@onet.eu>
2022-05-29 14:41:39 +0200

    add bottom navigation

--- app/src/main/java/com/example/boardgamegeekext/MainActivity.kt
+++ app/src/main/java/com/example/boardgamegeekext/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.boardgamegeekext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarMenuView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigation : BottomNavigationView;
+
+    private val homeFragment = HomeFragment();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        setCurrentFragment(homeFragment)
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment)
+            commit()
+        }
     }
 }
