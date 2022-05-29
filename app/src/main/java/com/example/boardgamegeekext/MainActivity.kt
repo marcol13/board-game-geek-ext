77862dc6627ae02a547cf0c8d554a83049ee2e03
Marcin <marcinkrueger@onet.eu>
2022-05-30 00:49:24 +0200

    settings view

--- app/src/main/java/com/example/boardgamegeekext/MainActivity.kt
+++ app/src/main/java/com/example/boardgamegeekext/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.boardgamegeekext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarMenuView
+import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bottomNavigation : BottomNavigationView;
+    private lateinit var bottomNavigation : BottomNavigationView
 
-    private val homeFragment = HomeFragment();
+    private val homeFragment = HomeFragment()
+    private val settingsFragment = SettingsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,15 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation = findViewById(R.id.bottom_navigation)
 
         setCurrentFragment(homeFragment)
+
+        bottomNavigation.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> setCurrentFragment(homeFragment)
+                R.id.nav_settings -> setCurrentFragment(settingsFragment)
+            }
+            true
+        })
+
 
     }
 
